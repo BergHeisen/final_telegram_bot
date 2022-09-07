7c413b1014a133059f8e98940da79ed11f35ace1
= <=>
2022-09-07 20:08:26 +0200

    Launch plog::init in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "plog/Appenders/ConsoleAppender.h"
+#include "plog/Formatters/TxtFormatter.h"
+#include "plog/Init.h"
 #include "telegram_client.hpp"
 #include "utils.hpp"
 #include "yt.hpp"
@@ -6,7 +9,10 @@
 #include <iostream>
 #include <unistd.h>
 
+static plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
+
 int main(int argc, char **argv) {
+  plog::init(plog::debug, &consoleAppender);
   std::string token = get_env_var_or_default(
       "TELEGRAM_TOKEN", "2053605815:AAHpGfk6hHnx1fRktpOmpHl_yCOkZesDOrk");
   TelegramClient client(token.c_str());
