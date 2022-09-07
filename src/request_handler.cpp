7c49d6e6d8ea7751efecc0ca4d635e31af9a6ea9
= <=>
2022-09-07 02:39:23 +0200

    Properly connect to Redis Server

--- src/request_handler.cpp
+++ src/request_handler.cpp
@@ -1,19 +1,50 @@
 #include "request_handler.hpp"
+#include "cpp_redis/core/types.hpp"
+#include "fmt/core.h"
 #include "nlohmann/json.hpp"
-#include <random>
-#include <type_traits>
-namespace {
+#include "plog/Appenders/ConsoleAppender.h"
+#include "plog/Formatters/TxtFormatter.h"
+#include "plog/Init.h"
+#include "plog/Logger.h"
+#include <memory>
+#include <plog/Log.h>
+#include <unistd.h>
 
-static RedisClient client;
-static std::default_random_engine engine;
-
-template <typename T,
-          typename std::enable_if<std::is_base_of<Request, T>::value>::type *>
-std::string createRequest(T &request) {
+RedisClient::RedisClient() {
+  std::string host = get_env_var_or_default("REDIS_HOST", "127.0.0.1");
+  std::string port = get_env_var_or_default("REDIS_PORT", "6379");
+  connect(host, std::stoi(port));
+  client->sync_commit();
+}
+void RedisClient::set(std::string &key, std::string &value) const {
+  auto future = client->set(key, value);
+  client->sync_commit();
+  const auto reply = future.get();
+  if (reply.is_error()) {
+    LOGE << fmt::format("Error occured with key: {} value: {}, error: {} ", key,
+                        value, reply.error());
+  } else {
+    LOGD << fmt::format("Setting key: {} value: {}, ok: {}", key, value,
+                        reply.ok());
+  }
+  return;
 }
 
-template <typename T, typename std::enable_if<
-                          std::is_base_of<Request, T>::value>::type * = nullptr>
-std::shared_ptr<T> getRequest(std::string &id) {
-  client.get(id);
+std::string RedisClient::get(std::string &key) const {
+  auto reply = client->get(key).get();
+  client->sync_commit();
+  if (reply.is_null() || reply.is_error()) {
+    return "";
+  } else {
+    return reply.as_string();
+  }
+}
+
+void RedisClient::connect(std::string &host, int port) const {
+  const cpp_redis::connect_callback_t idk;
+  client->connect(host, port,
+                  [](const std::string &host, std::size_t port,
+                     cpp_redis::connect_state status) {
+                    LOGD << fmt::format("Connect State on {}:{}", host, port);
+                  });
 }
--- src/request_handler.hpp
+++ src/request_handler.hpp
@@ -1,54 +1,62 @@
 #pragma once
+#include "utils.hpp"
+#include <BS_thread_pool.hpp>
+#include <climits>
 #include <cpp_redis/core/client.hpp>
+#include <cstdint>
 #include <memory>
 #include <nlohmann/json.hpp>
+#include <random>
 #include <string>
+#include <type_traits>
+
+static std::random_device engine;
+const int REQUEST_VERSION = 1;
+
 class RedisClient {
 private:
   std::unique_ptr<cpp_redis::client> client =
       std::make_unique<cpp_redis::client>();
+  RedisClient();
 
 public:
-  RedisClient() { client->connect(); }
-  void set(std::string &key, std::string &value) { client->set(key, value); }
-  std::string get(std::string &key) {
-    auto reply = client->get(key).get();
-    if (reply.is_null() || reply.is_error()) {
-      return "";
-    } else {
-      return reply.as_string();
-    }
-  }
+  void connect(std::string &host, int port) const;
+  void set(std::string &key, std::string &value) const;
+  std::string get(std::string &key) const;
+  // static std::shared_ptr<RedisClient> getInstance() {
+  static RedisClient &getInstance() {
+    // static std::shared_ptr<RedisClient> s{new RedisClient};
+    static RedisClient s;
+    return s;
+  };
 };
 
-struct Request {
-  std::string requestVersion;
-  std::string operation;
-  std::string fileIdentifier;
+struct DownloadRequest {
+  std::string fileIdentifier = "";
+  bool audioOnly = "false";
+  std::string resolution = "";
+  int requestVersion = 1;
+  std::string operation = "download";
 };
-
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Request, requestVersion, operation,
-                                   fileIdentifier);
-struct DownloadRequest : public Request {};
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(DownloadRequest, requestVersion, operation,
-                                   fileIdentifier);
+                                   fileIdentifier, audioOnly, resolution);
 
-template <typename T,
-          typename std::enable_if<std::is_base_of<Request, T>::value>::type *>
+template <typename T>
 
 std::string createRequest(T &request) {
+  RedisClient &client = RedisClient::getInstance();
   std::string id = std::to_string(
       std::uniform_int_distribution<long>(LONG_MIN, LONG_MAX)(engine));
-  // nlohmann::json r;
-  // r["requestVersion"] = request.requestVersion;
-  // r["operation"] = request.operation;
-  // r["fileIdentifier"] = request.fileIdentifier;
-  auto r = to_json(request);
+  nlohmann::json r{request};
   std::string requestJson = r.dump();
   client.set(id, requestJson);
   return id;
 }
 
-template <typename T,
-          typename std::enable_if<std::is_base_of<Request, T>::value>::type *>
-std::shared_ptr<T> getRequest(const char *id) {}
+template <typename T> std::shared_ptr<T> getRequest(std::string &id) {
+  const RedisClient &client = RedisClient::getInstance();
+  const std::string jsonString = client.get(id);
+  const nlohmann::json j{jsonString};
+  const auto idk = j.get<T>();
+  const auto request = std::make_shared<T>(j.get<T>());
+}
