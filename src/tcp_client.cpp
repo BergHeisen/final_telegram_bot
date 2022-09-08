fbd89133ef4bb440ecc90ec02f9059aaa2086a80
= <=>
2022-09-08 02:34:13 +0200

    Improve the tcp client class

--- src/tcp_client.cpp
+++ src/tcp_client.cpp
@@ -2,6 +2,7 @@
 #include "fmt/core.h"
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
+#include <cassert>
 #include <cstdint>
 #include <cstring>
 #include <netdb.h>
@@ -10,6 +11,7 @@
 #include <sstream>
 #include <strings.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 TCPConnection::TCPConnection(const std::string &host, const int port) {
   initiate_connection(host, port);
@@ -24,47 +26,79 @@ void TCPConnection::initiate_connection(const std::string &host,
     throw;
   }
   const int enable = 1;
-  hostent *host_t = gethostbyname(host.c_str());
+  // hostent *host_t = gethostbyname(host.c_str());
   sockaddr_in sendSockAddr;
-  bzero((char *)&sendSockAddr, sizeof(sendSockAddr));
+  bzero(&sendSockAddr, sizeof(sendSockAddr));
+  // bcopy(host_t->h_addr_list, &sendSockAddr.sin_addr, host_t->h_length);
   sendSockAddr.sin_family = AF_INET;
-  sendSockAddr.sin_addr.s_addr =
-      inet_addr(inet_ntoa(*(struct in_addr *)*host_t->h_addr_list));
-
-  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+  sendSockAddr.sin_port = htons(port);
+  sendSockAddr.sin_addr.s_addr = inet_addr(host.c_str());
+  // setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEADDR, &enable,
+  //            sizeof(int));
 
   int status = connect(sockfd, (sockaddr *)&sendSockAddr, sizeof(sendSockAddr));
   if (status < 0) {
-    LOGD << fmt::format("Unable to bind to host: {} port: {}", host, port);
+    LOGD << fmt::format("Unable to bind to host: {} port: {} errno: {}", host,
+                        port, status);
     throw;
   } else {
     LOGD << fmt::format("Successfully bound to host: {} port: {}", host, port);
     connected = true;
   };
 }
+void TCPConnection::sendData(void *data, const int dataLen) {
+  send(sockfd, &dataLen, sizeof(dataLen), 0);
+  int ok;
+  recv(sockfd, &ok, sizeof(int), 0);
+  assert(ok == dataLen);
+  send(sockfd, data, dataLen, 0);
+}
+
+void TCPConnection::receiveData(void *data, const int dataLenToRead) {
+  if (dataLenToRead < 2048) {
+    recv(sockfd, data, sizeof(dataLenToRead), 0);
+  } else {
+  }
+}
+void TCPConnection::receiveData(std::stringstream &ss,
+                                const int dataLenToRead) {
+  int bytes_read = 0;
+  char buf[2048];
+  while (dataLenToRead != bytes_read) {
+    memset(&buf, 0, sizeof(buf));
+    bytes_read += recv(sockfd, &buf, sizeof(buf), 0);
+    ss << buf;
+  }
+}
 
 std::string TCPConnection::exchangeMessage(const std::string &message) {
-  uint16_t bytesRead = 0;
-  uint16_t size = message.size();
-  uint16_t response_length;
+  int bytesRead = 0;
+  int size = message.size();
+  int response_length = 0;
   const char *messageCstring = message.c_str();
   std::stringstream ss;
-  send(sockfd, (char *)&size, sizeof(size), 0);
-  send(sockfd, (char *)&messageCstring, strlen(messageCstring), 0);
-  recv(sockfd, &response_length, sizeof(response_length), 0);
+  LOGD << fmt::format("size = {}", size);
+  sendData((void *)messageCstring, size);
+
+  receiveData(&response_length, 4);
+
   LOGD << fmt::format("Have to read {} bytes", response_length);
-  while (response_length != bytesRead) {
-    char buf[1500];
-    memset(&buf, 0, sizeof(buf));
-    bytesRead += recv(sockfd, &buf, sizeof(buf), 0);
-    ss << buf;
-    LOGD << fmt::format("Read now {} bytes", bytesRead);
-  }
-  LOGD << fmt::format("Server responded with: {}", ss.str());
+
+  receiveData(ss, response_length);
+  LOGD << fmt::format("Server responded with: {}", "success");
   return ss.str();
 }
 
+void TCPConnection::closeConnection() {
+  try {
+    const char *message = "closeConnection";
+    sendData((void *)message, strlen(message));
+  } catch (...) {
+  }
+}
+
 TCPConnection::~TCPConnection() {
+  closeConnection();
   if (this->connected) {
     close(sockfd);
   }
--- src/tcp_client.hpp
+++ src/tcp_client.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <sstream>
 #include <string>
 
 class TCPConnection {
@@ -7,6 +8,7 @@ class TCPConnection {
 private:
   int sockfd;
   bool connected = false;
+  void closeConnection();
 
   void initiate_connection(const std::string &host, const int port);
 
@@ -14,6 +16,9 @@ public:
   TCPConnection(const std::string &host, const int port);
   static std::shared_ptr<TCPConnection> instantiate(const std::string &host,
                                                     const int port);
+  void sendData(void *data, const int dataLen);
+  void receiveData(void *data, const int dataLenToRead);
+  void receiveData(std::stringstream &ss, const int dataLenToRead);
   std::string exchangeMessage(const std::string &message);
   ~TCPConnection();
 };
