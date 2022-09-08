32d1ca2630aaee3f7e7ffdcb729d15b58811c2f0
= <=>
2022-09-08 02:37:47 +0200

    Improve tcp test

--- test/test_tcp.cpp
+++ test/test_tcp.cpp
@@ -1,7 +1,28 @@
 #include "../src/tcp_client.hpp"
+#include <fstream>
 #include <gtest/gtest.h>
 #include <string>
 
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ostringstream;
+using std::string;
+
+string readFileIntoString(const string &path) {
+  ifstream input_file = ifstream(path);
+  if (!input_file.is_open()) {
+    cerr << "Could not open the file - '" << path << "'" << endl;
+    exit(EXIT_FAILURE);
+  }
+  return string((std::istreambuf_iterator<char>(input_file)),
+                std::istreambuf_iterator<char>());
+}
+
 TEST(TCPConnection, TCPConnection) {
-  auto connection = TCPConnection::instantiate("127.0.0.1", 3434);
+  auto connection = TCPConnection::instantiate("127.0.0.1", 3436);
+  // auto content = readFileIntoString("./info.json");
+  connection->exchangeMessage("Hello World");
+  std::cout << "Finished";
 }
